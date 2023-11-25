d0f9d23d87724fe38390a63e56d41cac4f165ae5
erez yaron <erez@yaron.com>
2023-11-25 10:47:35 -0500

    add Home and Routes instead of swtich

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import Background from './components/Background/Background';
-import Navbar from './components/Navbar/Navbar';
-import Header from './components/Header/Header';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Sections/Home'
+import Schedule from './Sections/Schedule';
+import Programs from './Sections/Programs';
+import About from './Sections/About';
+import GymRentals from './Sections/GymRentals';
+
 
 function App() {
   return (
-    <>
-      <Background />
-      <Header />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/schedule' element={<Schedule />} />
+        <Route path='/programs' element={<Programs />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/gym-rentals' element={<GymRentals/>} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ src/Sections/Home.jsx
@@ -0,0 +1,12 @@
+import Background from '../components/Background/Background'
+import Header from '../components/Header/Header';
+const Home = () => {
+    return (
+        <>
+        <Background />
+        <Header />
+        </>
+    )
+};
+
+export default Home
