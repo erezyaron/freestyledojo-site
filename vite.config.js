9e4c37f4be320409f2fdf5d9d2668bbbb2963a0f
erez yaron <erez@yaron.com>
2023-11-26 16:51:24 -0500

    update yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/freestyledojo-site/',
+  base: process.env.NODE_ENV === 'production' ? '/freestyledojo-site/' : '/',
   plugins: [react()],
 })
