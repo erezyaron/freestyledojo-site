a60c6d57890a1f4eeb07d5265df713fa094b193f
erez <erez.yaron@gmail.com>
2023-11-30 10:35:33 -0500

    under construction

---
+++ src/components/UnderConstruction/UnderConstruction.jsx
@@ -0,0 +1,16 @@
+import './UnderConstruction.css'
+
+function UnderConstruction() {
+  return (
+    <>
+    <div className='background'>
+        <div className='background-overlay'></div>
+        <div className='construction'>
+          Under Construction: We are updating our webiste<br/>Please email <a href="mailto:info@freestyledojo.ca">info@freestyledojo.ca</a> for assistance.
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default UnderConstruction
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import UnderConstruction from './components/UnderConstruction/UnderConstruction.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UnderConstruction />
   </React.StrictMode>,
 )
