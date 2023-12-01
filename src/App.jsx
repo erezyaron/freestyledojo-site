ffac4df1cbd7d2be20cfce3d862ac5bfae2bfe56
erez <erez.yaron@gmail.com>
2023-11-30 20:58:25 -0500

    changes around

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './Sections/Home'
-import Schedule from './Sections/Schedule';
-import Programs from './Sections/Programs';
-import About from './Sections/About';
-import GymRentals from './Sections/GymRentals';
-import './App.css'
+import * as React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
+import UnderConstruction from "./components/UnderConstruction/UnderConstruction.jsx";
+import Home from "./Sections/Home";
+import EventsPage from "./Sections/EventsPage.jsx";
+import JiuJitsuClasses from "./Sections/JiuJitsuClasses.jsx";
+import KickboxingClasses from "./Sections/KickboxingClasses.jsx";
+import FitnessClasses from "./Sections/FitnessClasses.jsx";
+import KidsClasses from "./Sections/KidsClasses.jsx";
+import GymRentalsPage from "./Sections/GymRentalsPage.jsx";
+import BookAClass from "./Sections/BookAClass.jsx";
+import AboutOurCoaches from "./Sections/AboutOurCoaches.jsx";
+import ClassSchedule from "./Sections/ClassSchedule.jsx";
 
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/schedule' element={<Schedule />} />
-        <Route path='/programs' element={<Programs />} />
-        <Route path='/about-out-coaches' element={<About/>} />
-        <Route path='/gym-rentals' element={<GymRentals/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/programs" element={<Home />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route
+          path="/toronto-jiu-jitsu-classes"
+          element={<JiuJitsuClasses />}
+        />
+        <Route
+          path="/toronto-kickboxing-classes"
+          element={<KickboxingClasses />}
+        />
+        <Route path="/toronto-fitness-classes" element={<FitnessClasses />} />
+        <Route path="/toronto-kids-martial-arts" element={<KidsClasses />} />
+        <Route path="/about-our-coaches" element={<AboutOurCoaches />} />
+        <Route path="/gym-rentals" element={<GymRentalsPage />} />
+        <Route path="/contact-free-trial-class" element={<Home />} />
+        <Route path="/book-a-class" element={<BookAClass />} />
+        <Route path="/schedule" element={<ClassSchedule />} />
       </Routes>
     </Router>
   )
-}
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-import UnderConstruction from "./components/UnderConstruction/UnderConstruction.jsx";
-import Home from "./Sections/Home";
-import EventsPage from "./Sections/EventsPage.jsx";
-import JiuJitsuClasses from "./Sections/JiuJitsuClasses.jsx";
-import KickboxingClasses from "./Sections/KickboxingClasses.jsx";
-import FitnessClasses from "./Sections/FitnessClasses.jsx";
-import KidsClasses from "./Sections/KidsClasses.jsx";
-import GymRentalsPage from "./Sections/GymRentalsPage.jsx";
-import BookAClass from "./Sections/BookAClass.jsx";
-import AboutOurCoaches from "./Sections/AboutOurCoaches.jsx";
-import ClassSchedule from "./Sections/ClassSchedule.jsx";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import  App  from './App';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<UnderConstruction />} />
-        <Route path="/programs" element={<Home />} />
-        <Route path="/events" element={<EventsPage />} />
-        <Route path="/toronto-jiu-jitsu-classes" element={<JiuJitsuClasses />} />
-        <Route path="/toronto-kickboxing-classes" element={<KickboxingClasses />} />
-        <Route path="/toronto-fitness-classes" element={<FitnessClasses />} />
-        <Route path="/toronto-kids-martial-arts" element={<KidsClasses />} />
-        <Route path="/about-our-coaches" element={<AboutOurCoaches />} />
-        <Route path="/gym-rentals" element={<GymRentalsPage />} />
-        <Route path="/contact-free-trial-class" element={<Home />} />
-        <Route path="/book-a-class" element={<BookAClass />} />
-        <Route path="/schedule" element={<ClassSchedule />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>
+      <App />
+  </React.StrictMode>,
 );
